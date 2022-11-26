6e0ae81021ae4c27b5ee096a486ead480404fad1
baejiin0506 <baejiin@nate.com>
2022-11-26 11:48:05 +0900

    1126

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,22 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var aa = '222222221'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+      {aa}
+      </div>
     </div>
   );
 }
